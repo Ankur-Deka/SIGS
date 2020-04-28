5164f27033c36db6b3ac714b6688d76fe54a7bcd
Ankur-Deka <ankurnet1996@gmail.com>
2020-04-27 20:03:42 -0400

    packaged

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="SIGS", # Replace with your own username
+    name="grid_search", # Replace with your own username
     version="0.0.1",
     author="Ankur",
     author_email="adeka@andrew.cmu.edu",
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Ankur-Deka/SIGS-Grid-Search",
-    packages=setuptools.find_packages(),
+    packages=['grid_search'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
